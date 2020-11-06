d0201a318c17182fb079be60bf5b135dc73283cb
Fajar AM <somabangsa@gmail.com>
2020-11-06 08:18:26 +0700

    update footer

---
+++ app/src/main/java/id/sch/kafila/catalog/components/KafilaFooter.java
@@ -0,0 +1,51 @@
+package id.sch.kafila.catalog.components;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.Nullable;
+import android.support.annotation.StyleableRes;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import id.sch.kafila.catalog.R;
+import id.sch.kafila.catalog.util.Logs;
+
+public class KafilaFooter extends RelativeLayout {
+
+    private ImageView webIcon;
+    private ImageView mailIcon;
+    private ImageView phoneIcon;
+
+    private TextView webLabel;
+    private TextView phoneLabel;
+    private TextView mailLabel;
+
+    public KafilaFooter(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        inflate(context, R.layout.kafila_footer, this);
+        initComponents();
+        for (int i = 0; i< attrs.getAttributeCount(); i++){
+
+            Logs.log(i," attr: ", attrs.getAttributeName(i),"=",attrs.getAttributeValue(i));
+        }
+
+    }
+
+    private void initComponents() {
+        webIcon = findViewById(R.id.footer_web_icon);
+        mailIcon = findViewById(R.id.footer_email_icon);
+        phoneIcon = findViewById(R.id.footer_phone_icon);
+
+        webLabel = findViewById(R.id.footer_web_label);
+        mailLabel = findViewById(R.id.footer_email_label);
+        phoneLabel = findViewById(R.id.footer_phone_label);
+    }
+
+
+}
